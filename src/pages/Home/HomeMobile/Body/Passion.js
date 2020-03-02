ced7c8c8ff905e190dda7e436a869ed122185e3f
tranphuongnam210699 <tranphuongnam21.06.99@gmail.com>
2020-03-02 23:34:42 +0700

    update code Home Page

--- src/pages/Home/HomeMobile/Body/Passion.js
+++ src/pages/Home/HomeMobile/Body/Passion.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 
+import Image from '../../../../assets/imageHomePage/HM Studio 2020.png';
+
 class Passion extends Component {
     render() {
         return (
@@ -7,17 +9,18 @@ class Passion extends Component {
                 <div className='titleHeader d-flex flex-column justify-content-center align-items-center'>
                     <span>Passion Led Us Here</span>
                     <span>
-                        Ngoài đam mê với áo quần, Tailor Wings mong muốn
-                        được đem giá trị về đúng người đã tạo ra nó.
-                        Quần áo bạn mặc là do bàn tay của người thợ may
-                        tạo ra, vì bạn trông thật tuyệt vời trong những
-                        bộ cánh ấy, nên họ hoàn toàn xứng đáng nhận được
-                        giá trị tương ứng. Hãy cùng Tailor Wings chắp
-                        cho những đôi cánh chưa từng được bay lên -
+                        Ngoài đam mê với áo quần, Tailor Wings mong muốn được
+                        đem giá trị về đúng người đã tạo ra nó. Quần áo bạn mặc
+                        là do bàn tay của người thợ may tạo ra, vì bạn trông
+                        thật tuyệt vời trong những bộ cánh ấy, nên họ hoàn toàn
+                        xứng đáng nhận được giá trị tương ứng. Hãy cùng Tailor
+                        Wings chắp cho những đôi cánh chưa từng được bay lên -
                         những người thợ may tận tụy.
-                            </span>
+                    </span>
                 </div>
-                <div className='video'></div>
+                <div className='video'>
+                    <img src={Image} />
+                </div>
                 <hr />
             </div>
         );
