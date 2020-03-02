d01b84e7737b6ebb045c6c560d09f22ab64d6f2f
tranphuongnam210699 <tranphuongnam21.06.99@gmail.com>
2020-03-02 15:55:06 +0700

    update page shopping cart

--- src/pages/ShoppingCart/ShoppingCart.js
+++ src/pages/ShoppingCart/ShoppingCart.js
@@ -1,96 +1,80 @@
 import React, { Component } from 'react';
-import "./ShoppingCart.scss";
-import ProductList from './ProductList';
-import Summary from './Summary';
-import CheckOutMobile from './CheckOutMobile';
-import { connect } from 'react-redux';
-import ReactGA from 'react-ga'
+import './ShoppingCart.scss';
+import { Icon, Select } from 'antd';
 
-const initGA = () => {
-    console.log('initGA');
-    ReactGA.initialize('UA-159143322-1')
-}
+const { Option } = Select;
 
-const logPageView = () => {
-    ReactGA.set({ page: window.location.pathname });
-    ReactGA.pageview(window.location.pathname + window.location.search)
-}
+const tmp = new Array(5).fill(1);
 
-export class ShoppingCart extends Component {
-    constructor(props) {
-        super(props);
-        window.scrollTo({
-            top: 0,
-            behavior: "smooth"
-        });
-        this.state = {
-            productsOnCart: JSON.parse(sessionStorage.getItem('productsOnCart')) || [],
-            totalProductsOnCart: 0,
-            subtotalPrice: 0,
-            isCartUpdated: false
-        }
-    }
-
-    componentDidMount() {
-        initGA()
-        logPageView()
-        let productsOnCart = JSON.parse(sessionStorage.getItem('productsOnCart')) || [];
-        let totalProductsOnCart = productsOnCart.reduce((accumulator, current) => {
-            return accumulator + current.quantity
-        }, 0);
-        let subtotalPrice = productsOnCart.reduce((accumulator, current) => {
-            return accumulator + (current.price * current.quantity)
-        }, 0);
-        this.setState({
-            productsOnCart,
-            totalProductsOnCart,
-            subtotalPrice
-        })
-    }
-
-    static getDerivedStateFromProps(nextProps, prevState) {
-        if (nextProps.isCartUpdated !== prevState.isCartUpdated) {
-            let productsOnCart = JSON.parse(sessionStorage.getItem('productsOnCart'));
-            let totalProductsOnCart = productsOnCart.reduce((accumulator, current) => {
-                return accumulator + current.quantity
-            }, 0);
-            let subtotalPrice = productsOnCart.reduce((accumulator, current) => {
-                return accumulator + (current.price * current.quantity)
-            }, 0);
-			return {
-                productsOnCart,
-                totalProductsOnCart,
-                subtotalPrice,
-				isCartUpdated: nextProps.isCartUpdated
-			}
-        }
-        return null;
-    }
-    
-
+export default class ShoppingCart extends Component {
     render() {
-        const { productsOnCart, totalProductsOnCart, subtotalPrice, isCartUpdated } = this.state;
         return (
-            <div className="pageMyCart">
-                <div className="mainPage">
-                    <div className="selectedProductList">
-                        <div className="row">
-                            <ProductList productsOnCart={productsOnCart} totalProductsOnCart={totalProductsOnCart} />
-                            <Summary subtotalPrice={subtotalPrice} totalProductsOnCart={totalProductsOnCart} />
+            <div className='pageMyCart-wraper'>
+                <div className='navbarHeader d-flex flex-row justify-content-between align-items-center'>
+                    <div className='iconBack '>
+                        <Icon type='left' />
+                    </div>
+                    <div className='titleHeader'>
+                        <span>Giỏ hàng</span>
+                    </div>
+                    <div className='divClone'></div>
+                </div>
+                {tmp.map((a, index) => {
+                    console.log('a', a);
+                    return (
+                        <div className='product d-flex flex-row' key={index}>
+                            <div className='col-5'>
+                                <div className='image'></div>
+                                <div className='buttonWishList d-flex flex-row justify-content-center align-items-center'>
+                                    <Icon type='heart' />
+                                </div>
+                            </div>
+                            <div className='col-7 d-flex flex-column justify-content-between'>
+                                <div className='content-wraper'>
+                                    <div className='iconDelete d-flex flex-row justify-content-end  '>
+                                        <Icon type='close' />
+                                    </div>
+                                    <div className='contentHead d-flex flex-column'>
+                                        <span>Đầm công chúa</span>
+                                        <span>240.000 VNĐ</span>
+                                    </div>
+                                    <div className='bodyMetric d-flex flex-column'>
+                                        <span>Eo 99</span>
+                                        <span>Ngực 82</span>
+                                        <div className='d-flex flex-row justify-content-between'>
+                                            <span>Mông 103</span>
+                                            <span>Size XL</span>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className='changeQuantity'>
+                                    <Select
+                                        defaultValue='1'
+                                        style={{ width: '100%' }}
+                                    >
+                                        <Option value='1'>1</Option>
+                                        <Option value='2'>2</Option>
+                                        <Option value='3'>3</Option>
+                                    </Select>
+                                </div>
+                            </div>
                         </div>
+                    );
+                })}
+
+                <div className='checkOutFixBottom'>
+                    <div className='title d-flex flex-row justify-content-between align-items-center'>
+                        <span>Tạm tính</span>
+                        <span>620.000 VNĐ</span>
+                    </div>
+                    <div className='button d-flex flex-row align-items-center justify-content-center'>
+                        <span>
+                            Thanh toán
+                            <Icon type='right' />
+                        </span>
                     </div>
                 </div>
-                {/* mobile */}
-                <CheckOutMobile subtotalPrice={subtotalPrice} totalProductsOnCart={totalProductsOnCart} />
             </div>
         );
     }
 }
-
-const mapStateToProps = (state) => {
-	return {
-		isCartUpdated: state.updateProductOnCart
-	}
-}
-
-export default connect(mapStateToProps, null)(ShoppingCart);
