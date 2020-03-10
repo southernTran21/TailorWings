f6235b56cadbe5bf54f04e970d090935702f144a
tranphuongnam210699 <tranphuongnam21.06.99@gmail.com>
2020-03-10 21:50:50 +0700

    edited FourSteps.js

--- src/pages/Home/HomeMobile/Body/FourSteps.js
+++ src/pages/Home/HomeMobile/Body/FourSteps.js
@@ -1,43 +1,60 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+
+import Step1 from "../../../../assets/imageHomePage/step 1.svg";
+import Step2 from "../../../../assets/imageHomePage/step 2.svg";
+import Step3 from "../../../../assets/imageHomePage/step 3.svg";
+import Step4 from "../../../../assets/imageHomePage/step 4.svg";
 
 class FourSteps extends Component {
     render() {
         return (
-            <div className='content__bodyPage d-flex flex-column justify-content-around'>
-                <div className='title d-flex flex-column'>
+            <div className="content__bodyPage d-flex flex-column justify-content-around">
+                <div className="title d-flex flex-column">
                     <span>Đặt may giao tận nhà chỉ với 4 bước đơn giản</span>
-                    <span>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                        Aenean in nisl in ex varius luctus.
-                    </span>
                 </div>
-                <div className='fourStep-wraper d-flex flex-row justify-content-between'>
-                    <div className='fourStep-content'>
-                        <div className='image'></div>
-                        <div className='title'>
+                <div className="fourStep-wraper d-flex flex-row justify-content-between align-items-center">
+                    <div className="fourStep-content">
+                        <div className="image">
+                            <img src={Step1} />
+                        </div>
+                        <div className="title">
                             <span>Chọn mẫu</span>
                         </div>
                     </div>
-                    <div className='fourStep-content'>
-                        <div className='image'></div>
-                        <div className='title'>
+                    <div className="fourStep-content">
+                        <div className="image">
+                            <img src={Step2} />
+                        </div>
+                        <div className="title">
                             <span>Chọn vải</span>
                         </div>
                     </div>
-                    <div className='fourStep-content'>
-                        <div className='image'></div>
-                        <div className='title'>
+                    <div className="fourStep-content">
+                        <div className="image">
+                            <img src={Step3} />
+                        </div>
+                        <div className="title">
                             <span>Chọn size</span>
                         </div>
                     </div>
-                    <div className='fourStep-content'>
-                        <div className='image'></div>
-                        <div className='title'>
+                    <div className="fourStep-content">
+                        <div className="image">
+                            <img src={Step4} />
+                        </div>
+                        <div className="title">
                             <span>Đặt hàng</span>
                         </div>
                     </div>
                 </div>
-                <hr />
+                <div className='numberStep d-flex flex-lg-row justify-content-center align-items-center'>
+                    <div className='number'>1</div>
+                    <hr />
+                    <div className='number'>2</div>
+                    <hr />
+                    <div className='number'>3</div>
+                    <hr />
+                    <div className='number'>4</div>
+                </div>
             </div>
         );
     }
