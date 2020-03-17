7ff1054f2d7d76877ac63c25375235af0c4f1edb
tranphuongnam210699 <tranphuongnam21.06.99@gmail.com>
2020-03-17 18:53:00 +0700

    up homePage version web

--- src/pages/Home/HomeWeb/Index.js
+++ src/pages/Home/HomeWeb/Index.js
@@ -14,6 +14,17 @@ import stepOne from "../.../../../../assets/imageHomePage/step 1.svg";
 import stepTwo from "../.../../../../assets/imageHomePage/step 2.svg";
 import stepThree from "../.../../../../assets/imageHomePage/step 3.svg";
 import stepFour from "../.../../../../assets/imageHomePage/step 4.svg";
+import imageCollection1 from "../.../../../../assets/imageHomePage/collection1.png";
+import imageCollection2 from "../.../../../../assets/imageHomePage/collection2.png";
+import imageCollection3 from "../.../../../../assets/imageHomePage/collection3.png";
+import iconEnter from "../.../../../../assets/imageHomePage/iconEnter.svg";
+import iconVectorRight from "../.../../../../assets/imageHomePage/vectorRight.svg";
+import imageCollection from "../.../../../../assets/imageHomePage/imageBST.jpg";
+import iconIG from "../.../../../../assets/imageHomePage/instagram.svg";
+import iconFB from "../.../../../../assets/imageHomePage/facebook.svg";
+import iconYTB from "../.../../../../assets/imageHomePage/youtube.svg";
+import iconIG1 from "../.../../../../assets/imageHomePage/instagram (1).svg";
+import logoTailorWings from "../.../../../../assets/imageHomePage/tailorwings.svg";
 
 const { Search } = Input;
 const { SubMenu } = Menu;
@@ -135,7 +146,7 @@ export default class HomeWeb extends Component {
                         </div>
                     </div>
                 </div>
-                <div className="fourStep_wrapper d-flex flex-column align-items-center">
+                <div className="fourStep_wrapper d-flex flex-column align-items-center fontMontserrat">
                     <div className="titleHeader">
                         <span>
                             Đặt may giao tận nhà chỉ với 4 bước đơn giản.
@@ -184,8 +195,222 @@ export default class HomeWeb extends Component {
                         <hr />
                         <div className="number">4</div>
                     </div>
-                    <div className='button d-flex flex-row justify-content-center align-items-center'>
+                    <div className="button d-flex flex-row justify-content-center align-items-center">
                         <span>ĐẶT MAY NGAY</span>
+                    </div>
+                </div>
+                <div className="striking_wrapper d-flex fontMontserrat">
+                    <div className="col-7">
+                        <div className="strikingfirst d-flex">
+                            <div className="image">
+                                <img src={imageCollection1} alt="" />
+                            </div>
+                            <div className="title d-flex flex-column">
+                                <span>Đầm Suông</span>
+                                <span>
+                                    Phục vụ với mọi ngữ cảnh như dự tiệc, công
+                                    sở và đi chơi…
+                                </span>
+                                <div className="image d-flex ">
+                                    <img src={iconEnter} alt="" />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-5 d-flex flex-column">
+                        <div className="strikingSecond">
+                            <div className="image"></div>
+                            <div className="titleFirst d-flex flex-column">
+                                <span>Đầm Ôm</span>
+                                <span>
+                                    Xu hướng đang thịnh ở các cửa hàng bán quần
+                                    áo.
+                                </span>
+                                <img src={iconEnter} alt="" />
+                            </div>
+                        </div>
+                        <div className="strikingSecond">
+                            <span className="titleSecond d-flex flex-column">
+                                <span>Đầm Xòe</span>
+                                <span>
+                                    Được sử dụng rộng rãi & ứng dụng nhiều trong
+                                    cuộc sống.
+                                </span>
+                                <img src={iconEnter} alt="" />
+                            </span>
+                        </div>
+                        <div className="image"></div>
+                    </div>
+                </div>
+                <div className="weYour d-flex flex-column justify-content-center align-items-center fontMontserrat">
+                    <span>We Tailor Your Wings</span>
+                    <span>
+                        Tự do lựa chọn để thể hiện phong cách của chính bạn.{" "}
+                        <span className="text-bold">
+                            Tất cả có trên cùng một ứng dụng.
+                        </span>
+                    </span>
+                </div>
+                <div className="collection_wrapper d-flex fontMontserrat">
+                    <div className="collection d-flex flex-column justify-content-center">
+                        <span>Bộ Sưu Tập</span>
+                        <svg height="45.18vh" width="14.16vw">
+                            <line
+                                x1="0"
+                                y1="0"
+                                x2="14.16vw"
+                                y2="45.18vh"
+                                style={{
+                                    stroke: "#111e6c",
+                                    strokeWidth: "1.2"
+                                }}
+                            />
+                        </svg>
+                        <span>
+                            Khám phá ngay những bộ sưu tập mới nhất từ nhà may.
+                        </span>
+                    </div>
+                    <div className="selectionCollection d-flex">
+                        <div className="selection d-flex">
+                            <div className="image">
+                                <img src={imageCollection} alt="" />
+                            </div>
+                            <div className="title d-flex flex-column align-items-center">
+                                <span>
+                                    Đầm<br></br> Dạo Phố
+                                </span>
+                                <span>
+                                    XEM NGAY{" "}
+                                    <img src={iconVectorRight} alt="" />
+                                </span>
+                            </div>
+                        </div>
+                        <div
+                            className="selection d-flex"
+                            style={{
+                                marginLeft: "1.66vw",
+                                marginRight: "1.66vw"
+                            }}
+                        >
+                            <div className="image">
+                                <img src={imageCollection} alt="" />
+                            </div>
+                            <div className="title d-flex flex-column align-items-center">
+                                <span>
+                                    Đầm <br></br> Công Sở
+                                </span>
+                                <span>
+                                    XEM NGAY{" "}
+                                    <img src={iconVectorRight} alt="" />
+                                </span>
+                            </div>
+                        </div>
+                        <div className="selection d-flex">
+                            <div className="image">
+                                <img src={imageCollection} alt="" />
+                            </div>
+                            <div className="title d-flex flex-column align-items-center">
+                                <span>
+                                    Đầm <br></br> Dự Tiệc
+                                </span>
+                                <span>
+                                    XEM NGAY{" "}
+                                    <img src={iconVectorRight} alt="" />
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="topList d-flex flex-column align-items-center fontMontserrat">
+                    <div className="title">
+                        <span>Nổi bật trong tuần</span>
+                    </div>
+                </div>
+                <div className="weGive d-flex flex-column align-items-center fontMontserrat">
+                    <div className="title d-flex flex-column align-items-center">
+                        <span>We Give Tailor Wings</span>
+                        <span>
+                            Trở thành đối tác của Tailor Wings để làm chủ cuộc
+                            sống của mình tốt hơn.
+                        </span>
+                    </div>
+                    <div className="image d-flex justify-content-between align-items-center">
+                        <img src="" alt="" />
+                        <img src="" alt="" />
+                        <img src="" alt="" />
+                    </div>
+                    <div className="button">
+                        <span>KẾT NỐI NGAY</span>
+                    </div>
+                </div>
+                <div className="passion d-flex fontMontserrat">
+                    <div className="title d-flex flex-column">
+                        <span>
+                            Passion <br></br> Led Us Here
+                        </span>
+                        <span>
+                            Quần áo bạn mặc là do bàn tay của người thợ may tạo
+                            ra. Hãy cùng Tailor Wings chắp cho những đôi cánh
+                            chưa từng được bay lên - những người thợ may tận
+                            tụy.
+                        </span>
+                    </div>
+                    <div className="image"></div>
+                </div>
+                <div className="instagram fontMontserrat">
+                    <div className="title d-flex align-items-center">
+                        <img src={iconIG} alt="instagram" />
+                        <span>Instagram</span>
+                    </div>
+                    <div className="crouselImage d-flex">
+                        <div className="image"></div>
+                        <div
+                            className="image"
+                            style={{ margin: "0 0.83vw" }}
+                        ></div>
+                        <div className="image"></div>
+                    </div>
+                </div>
+                <div className="footer_wrapper fontMontserrat">
+                    <div className="d-flex justify-content-between">
+                        <div className="info d-flex flex-column">
+                            <span>VỀ CHÚNG TÔI</span>
+                            <span>CHÍNH SÁCH HỖ TRỢ</span>
+                            <span>LIÊN HỆ</span>
+                        </div>
+                        <div className="contact">
+                            <span>
+                                HOTLINE 0939929405<br></br>wetailor@gmail.com
+                            </span>
+                            <div className="icon">
+                                <img src={iconYTB} alt="" />
+                                <img src={iconFB} alt="" />
+                                <img src={iconIG1} alt="" />
+                            </div>
+                        </div>
+                        <div className="logo">
+                            <img src={logoTailorWings} alt="" />
+                        </div>
+                        <div className="email">
+                            <div className="title">
+                                <span>
+                                    Đăng kí để <br></br>nhận ưu đãi mới nhất
+                                </span>
+                            </div>
+                            <div className="d-flex input">
+                                <Input placeholder="Email của bạn" />
+                                <div className="button d-flex justify-content-center align-items-center">
+                                    <span>ĐĂNG KÝ </span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="endFooter d-flex flex-column">
+                        <span>© 2020 Tailor Wings</span>
+                        <span>
+                            Công ty TNHH Tailor Wings với số đăng ký kinh doanh:
+                            0105777650
+                        </span>
                     </div>
                 </div>
             </div>
