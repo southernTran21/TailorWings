d12ead8e2261b89d87bcee6c018e472943ee3aa0
tranphuongnam210699 <tranphuongnam21.06.99@gmail.com>
2020-03-29 21:05:52 +0700

    updated code

--- src/components/NavBar/NavBarWeb/index.js
+++ src/components/NavBar/NavBarWeb/index.js
@@ -1,13 +1,14 @@
 import React, { Component } from "react";
 import { Icon } from "antd";
 import { Link } from "react-router-dom";
-import classNames from 'classnames'
+import classNames from "classnames";
 //
 import "./NavBarWeb.scss";
 import iconMenu from "../../../assets/productDetailWeb/menu-2.svg";
 import iconSearch from "../../../assets/productDetailWeb/search.svg";
 import iconCart from "../../../assets/productDetailWeb/cart.svg";
 import iconLogoTailorWings from "../../../assets/imageHomePage/Logo Header.svg";
+import iconClose from "../../../assets/imageHomePage/close.svg";
 import Backdrop from "../../SideBar/Backdrop";
 import SideBar from "../../SideBar/SideBar";
 
@@ -18,7 +19,7 @@ class NavBarWeb extends Component {
         this.state = {
             isSideBarOpen: false,
             isSearchOpen: false,
-            isProductDetailPage: currentPage === '/product-detail'
+            isProductDetailPage: currentPage === "/product-detail"
         };
     }
 
@@ -38,9 +39,9 @@ class NavBarWeb extends Component {
     sideBarIconChange = () => {
         const { isSideBarOpen } = this.state;
         if (isSideBarOpen) {
-            return <Icon type="close" onClick={this.sideBarOpen} />;
+            return <img src={iconClose} alt="" onClick={this.sideBarOpen} />;
         } else {
-            return <img src={iconMenu} alt="" onClick={this.sideBarOpen}/>;
+            return <img src={iconMenu} alt="" onClick={this.sideBarOpen} />;
         }
     };
 
@@ -67,13 +68,11 @@ class NavBarWeb extends Component {
         }
         let backdrop;
         if (this.state.isSideBarOpen) {
-            backdrop = <Backdrop click={this.backdropClickHandler} />
+            backdrop = <Backdrop click={this.backdropClickHandler} />;
         }
         return (
             <div className="navbarWeb d-flex align-items-center justify-content-between">
-                <div className="hamburgerMenu">
-                    {this.sideBarIconChange()}
-                </div>
+                <div className="hamburgerMenu">{this.sideBarIconChange()}</div>
                 <div
                     className="logoTailorWings"
                     onClick={() => this.props.history.push("/")}
@@ -81,7 +80,11 @@ class NavBarWeb extends Component {
                     <img src={iconLogoTailorWings} alt="" />
                 </div>
                 <div className="Tools d-flex">
-                    <div className={classNames("search", { unvisible: isProductDetailPage })}>
+                    <div
+                        className={classNames("search", {
+                            unvisible: isProductDetailPage
+                        })}
+                    >
                         <img src={iconSearch} alt="" />
                         <span>TÌM KIẾM</span>
                     </div>
