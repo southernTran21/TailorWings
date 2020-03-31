d7b6dd8904c5e5c8463fe8a347c93a8e3693a9bf
tranphuongnam210699 <tranphuongnam21.06.99@gmail.com>
2020-03-31 13:36:26 +0700

    updated code NavBar page support and Policy

--- src/pages/Policy/NavBar.js
+++ src/pages/Policy/NavBar.js
@@ -1,10 +1,24 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import "./Policy.scss";
+import { Icon, Badge } from "antd";
+import logoTW from "../../assets/imageHomePage/Logo Header.svg";
 
 class NavBar extends Component {
     render() {
         return (
-            <div>
-                
+            <div className="navbarHeader d-flex justify-content-between">
+                <div className='iconMenu d-flex align-items-center'>
+                    <Icon type="menu" />
+                </div>
+                <div className='titleHeader'>
+                    <img src={logoTW} alt="" />
+                </div>
+                <div className="iconShoppingCart d-flex justify-content-between align-items-center">
+                    <Icon type="shopping-cart" />
+                    <Badge count='1'>
+                        <a className="head-example" />
+                    </Badge>
+                </div>
             </div>
         );
     }
