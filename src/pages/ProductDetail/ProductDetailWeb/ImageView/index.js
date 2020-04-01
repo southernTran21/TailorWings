3a3759c7610a443e92c2aab0f08a649809fa99ca
Le Chau Huu Thien <lechauhuuthien96@mail.com>
2020-04-01 17:09:57 +0700

    Fix bug of image view at Product Detail page

--- src/pages/ProductDetail/ProductDetailWeb/ImageView/index.js
+++ src/pages/ProductDetail/ProductDetailWeb/ImageView/index.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { Button, Icon } from "antd";
 import Swiper from "react-id-swiper";
 import "./ImageView.scss";
+import Slider from "react-slick";
 
 export default class ImageView extends Component {
     constructor(props) {
@@ -39,35 +40,35 @@ export default class ImageView extends Component {
         const { productImages, productName } = this.props;
         let isEmpty = productImages.every(image => image === "");
         if (productImages != null && !isEmpty) {
-            const params = {
-                slidesPerView: 1,
-                // loop: true,
-                // centeredSlides: true,
-            };
             // const params = {
-            //     pagination: {
-            //         el: ".swiper-pagination",
-            //         type: "fraction"
-            //     },
-            //     navigation: {
-            //         nextEl: ".swiper-button-next",
-            //         prevEl: ".swiper-button-prev"
-            //     }
+            // slidesPerView: 1,
+            // space: 20,
+            // loop: true,
+            // centeredSlides: true,
             // };
             console.log("productImages :", productImages);
             return (
                 <Swiper
-                    // className="d-flex flex-column align-items-center"
-                    {...params}
+                    className="d-flex flex-column align-items-center"
+                    // {...params}
                     getSwiper={this.getSwiper.bind(this)}
                 >
-                    {productImages.map((image, index) => {
+                    {/* {productImages.map((image, index) => {
                         return (
-                            <div key={index} className="image-wraper">
+                            <div key={index} className="image-wrapper">
                                 <img src={image} alt={productName} />
                             </div>
                         );
-                    })}
+                    })} */}
+                    <div className="image-wrapper">
+                        {/* <img src={productImages[0]} alt={productName} /> */}
+                    </div>
+                    <div className="image-wrapper">
+                        {/* <img src={productImages[1]} alt={productName} /> */}
+                    </div>
+                    <div className="image-wrapper">
+                        {/* <img src={productImages[2]} alt={productName} /> */}
+                    </div>
                 </Swiper>
             );
         } else {
@@ -76,29 +77,33 @@ export default class ImageView extends Component {
     };
 
     render() {
+        const { productImages, productName } = this.props;
+        const settings = {
+            dots: true,
+            infinite: true,
+            speed: 500,
+            slidesToShow: 1,
+            slidesToScroll: 1
+        };
         return (
             <div className="imageView-wrapper">
-                <div className="d-flex justify-content-between">
+                <div className="back-btn d-flex justify-content-end">
                     <Button
                         onClick={() => this.props.onImageView(false)}
                         className="buttonBack"
                     >
                         <Icon type="close" />
                     </Button>
-                    <Button
-                        onClick={() => this.onSlide("prev")}
-                        className="buttonPrev"
-                    >
-                        <Icon type="left" />
-                    </Button>
-                    <Button
-                        onClick={() => this.onSlide("next")}
-                        className="buttonNext"
-                    >
-                        <Icon type="right" />
-                    </Button>
                 </div>
-                <div className="swiper">{this.renderContentHandling()}</div>
+                <Slider {...settings}>
+                    {productImages.map((image, index) => {
+                        return (
+                            <div key={index} className='image-wrapper'>
+                                <img src={image} alt={productName} />
+                            </div>
+                        );
+                    })}
+                </Slider>
             </div>
         );
     }
