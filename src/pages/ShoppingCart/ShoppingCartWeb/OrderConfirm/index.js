72e332a97b2e0851176436ff852ffb0e35908d2f
Le Chau Huu Thien <lechauhuuthien96@mail.com>
2020-04-06 14:13:49 +0700

    logic for OrderConfirm page

---
+++ src/pages/ShoppingCart/ShoppingCartWeb/OrderConfirm/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class OrderConfirm extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
