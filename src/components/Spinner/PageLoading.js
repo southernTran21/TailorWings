e167020b8d04debd9ac78ef51ba6145a7c767028
tranphuongnam210699 <tranphuongnam21.06.99@gmail.com>
2020-04-07 16:36:30 +0700

    commit spinner

--- src/components/Spinner/PageLoading.js
+++ src/components/Spinner/PageLoading.js
@@ -1,11 +1,50 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import "./PageLoading.scss";
 
 export default class PageLoading extends Component {
     render() {
         return (
-            <div>
-                Loading...
+            <div className="spinner_wrapper">
+                {/* <div className="lds-ripple">
+                    <div></div>
+                    <div></div>
+                </div> */}
+                <div id="fountainTextG">
+                    <div id="fountainTextG_1" className="fountainTextG">
+                        T
+                    </div>
+                    <div id="fountainTextG_2" className="fountainTextG">
+                        A
+                    </div>
+                    <div id="fountainTextG_3" className="fountainTextG">
+                        I
+                    </div>
+                    <div id="fountainTextG_4" className="fountainTextG">
+                        L
+                    </div>
+                    <div id="fountainTextG_5" className="fountainTextG">
+                        O
+                    </div>
+                    <div id="fountainTextG_6" className="fountainTextG">
+                        R
+                    </div>
+                    <div id="fountainTextG_7" className="fountainTextG">
+                        W
+                    </div>
+                    <div id="fountainTextG_8" className="fountainTextG">
+                        I
+                    </div>
+                    <div id="fountainTextG_9" className="fountainTextG">
+                        N
+                    </div>
+                    <div id="fountainTextG_10" className="fountainTextG">
+                        G
+                    </div>
+                    <div id="fountainTextG_11" className="fountainTextG">
+                        S
+                    </div>
+                </div>
             </div>
-        )
+        );
     }
 }
