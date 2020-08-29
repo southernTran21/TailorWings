affc9a985a8f768c5ef4f18b76f12b24fc2ef20e
tranphuongnam210699 <tranphuongnam21.06.99@gmail.com>
2020-08-29 14:08:34 +0700

    develop UI button Back to Top

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -9,55 +9,57 @@ Footer.propTypes = {};
 
 function Footer(props) {
     return (
-        <div className="c-footer">
-            <div className="c-footer__section">
-                <section className="c-footer__section1">
-                    <div className="c-footer__option">
-                        <span className="c-footer__title">
-                            ĐIỀU KIỆN & ĐIỀU KHOẢN
-                        </span>
-                        <span className="c-footer__title">VỀ TAILOR WINGS</span>
-                        <span className="c-footer__title">LIÊN HỆ</span>
-                    </div>
-                    <div className="c-footer__social-icon">
-                        <img
-                            src={IconFaceBook}
-                            alt=""
-                            className="c-footer__fb"
-                        />
-                        <img
-                            src={IconInstagram}
-                            alt=""
-                            className="c-footer__ig"
-                        />
-                        <img
-                            src={IconYoutube}
-                            alt=""
-                            className="c-footer__yt"
-                        />
-                    </div>
-                </section>
-                <section className="c-footer__section2">
-                    <div className="c-footer__info">
-                        <span className="c-footer__text">
-                            HOTLINE 0902 541 398
-                        </span>
-                        <span className="c-footer__text">
-                            info.tailorwings@gmail.com
-                        </span>
-                    </div>
-                    <div className="c-footer__logo">
-                        <img src={LogoBird} alt="" />
-                    </div>
+        <div className='c-footer--wrapper'>
+            <div className="c-footer">
+                <div className="c-footer__section">
+                    <section className="c-footer__section1">
+                        <div className="c-footer__option">
+                            <span className="c-footer__title">
+                                ĐIỀU KIỆN & ĐIỀU KHOẢN
+                            </span>
+                            <span className="c-footer__title">VỀ TAILOR WINGS</span>
+                            <span className="c-footer__title">LIÊN HỆ</span>
+                        </div>
+                        <div className="c-footer__social-icon">
+                            <img
+                                src={IconFaceBook}
+                                alt=""
+                                className="c-footer__fb"
+                            />
+                            <img
+                                src={IconInstagram}
+                                alt=""
+                                className="c-footer__ig"
+                            />
+                            <img
+                                src={IconYoutube}
+                                alt=""
+                                className="c-footer__yt"
+                            />
+                        </div>
+                    </section>
+                    <section className="c-footer__section2">
+                        <div className="c-footer__info">
+                            <span className="c-footer__text">
+                                HOTLINE 0902 541 398
+                            </span>
+                            <span className="c-footer__text">
+                                info.tailorwings@gmail.com
+                            </span>
+                        </div>
+                        <div className="c-footer__logo">
+                            <img src={LogoBird} alt="" />
+                        </div>
+                    </section>
+                </div>
+                <section className="c-footer__section3">
+                    <span className="c-footer__text">© 2020 Tailor Wings</span>
+                    <span className="c-footer__text">
+                        Công ty TNHH Thương Mại Kết Nối Á Âu / Số đăng ký kinh doanh
+                        : 0313114301
+                    </span>
                 </section>
             </div>
-            <section className="c-footer__section3">
-                <span className="c-footer__text">© 2020 Tailor Wings</span>
-                <span className="c-footer__text">
-                    Công ty TNHH Thương Mại Kết Nối Á Âu / Số đăng ký kinh doanh
-                    : 0313114301
-                </span>
-            </section>
         </div>
     );
 }
